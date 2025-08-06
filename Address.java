e4ca52bef02b1fbbd2a3751e5e02dea8cc59a93a
Vanshtk <vanshthakur19225@gmail.com>
2025-08-06 22:55:20 +0530

    inital commit

--- Address.java
+++ Address.java
@@ -2,5 +2,7 @@ import java.util.*;
 public class Address{
     public static void main(String[]agrs){
     System.out.println("alpha2");
+    System.out.print("greater noida");
+    System.out.print("hblock-335");
     } 
 }
--- addition.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class addition{
-    public static void main(String[] agrs){
-        int a=5;
-        int b=4;
-        int sum= a+b;
-        System.out.println(sum);
-    }
-}
